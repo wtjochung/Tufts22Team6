7b0e36f2beec80dad86dc35363bad97675c0abca
Jo Chung <chjoanna@163.com>
2022-03-26 03:35:19 -0400

    Add PlayerInput system

---
+++ IfLooksCouldKill/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    public float speed = 0;
+    private float movementX, movementY;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        //todo see first person tutorial and change
+        rb.AddForce(movement * speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
