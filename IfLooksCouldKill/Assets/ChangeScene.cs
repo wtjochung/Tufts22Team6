515229ff97621c2f5acfd1559b6c7ae22020e39b
Jo Chung <chjoanna@163.com>
2022-04-18 23:57:53 -0400

    Added Title Screen
    -Start button links to LaserEyesTest scene
    -Red blur starts after player pressed the start button

---
+++ IfLooksCouldKill/Assets/ChangeScene.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class ChangeScene: MonoBehaviour
+{
+    public string sceneToLoad = "";
+    public GameObject item;
+    private RawImage a;
+    private Color32 endColor;
+
+    public void Start()
+    {
+        a = item.GetComponent<RawImage>();
+        endColor = new Color32(255, 0, 0, 100);
+    }
+
+    public void SetScene()
+    {
+        StartCoroutine(changeOpacity());
+
+        
+    }
+
+    public void changeScene(string sceneName)
+    {
+        Time.timeScale = 1f;
+        if (sceneName != "") SceneManager.LoadScene(sceneName);
+    }
+
+    private IEnumerator changeOpacity()
+    {
+        
+
+        float tick = 0f;
+        while (a.color != endColor)
+        {
+            tick += Time.deltaTime * 0.001f;
+            a.color = Color.Lerp(a.color, endColor, tick);
+            yield return null;
+        }
+        if (a.color == endColor)
+        {
+            if (sceneToLoad != "") SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
