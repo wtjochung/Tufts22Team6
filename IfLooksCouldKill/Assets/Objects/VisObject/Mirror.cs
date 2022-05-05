875817eaa81866e229bbd49bc335958ed0633a25
Henry Nissen <63805100+Ayerbe-Dev@users.noreply.github.com>
2022-05-05 12:48:20 -0400

    E

--- IfLooksCouldKill/Assets/Objects/VisObject/Mirror.cs
+++ IfLooksCouldKill/Assets/Objects/VisObject/Mirror.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 public class Mirror : MonoBehaviour {
     public Material material;
     public RenderTexture render_texture;
+    Renderer obj_renderer;
 
     void Start() {
+        obj_renderer = GetComponent<Renderer>();
         material = new Material(material); //Create a copy of the provided material
-        GetComponent<MeshRenderer>().material = material; //Set the object to use this instance of the material
-        render_texture = new RenderTexture(1280, 720, 1000); //Create a new render texture
+        render_texture = new RenderTexture(1280, 720, 1); //Create a new render texture
     }
 
     void OnRenderImage(RenderTexture src, RenderTexture dest) {
         Graphics.Blit(src, render_texture); //Write to the texture
         if (!GameManager.blind) {
             material.SetTexture("Texture", render_texture); //Set the material instance to use the texture we wrote to
+            obj_renderer.material = material; //Set the object to use this instance of the material
         }
     }
 }
