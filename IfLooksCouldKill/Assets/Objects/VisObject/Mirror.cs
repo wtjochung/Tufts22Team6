ed9e51ca3f1dd81682a0ba84d35d338ba8d21a68
Henry Nissen <63805100+Ayerbe-Dev@users.noreply.github.com>
2022-05-05 13:15:59 -0400

    got the mirrors to work

--- IfLooksCouldKill/Assets/Objects/VisObject/Mirror.cs
+++ IfLooksCouldKill/Assets/Objects/VisObject/Mirror.cs
@@ -5,19 +5,21 @@ using UnityEngine;
 public class Mirror : MonoBehaviour {
     public Material material;
     public RenderTexture render_texture;
-    Renderer obj_renderer;
+    public Transform target;
 
     void Start() {
-        obj_renderer = GetComponent<Renderer>();
-        material = new Material(material); //Create a copy of the provided material
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        material = new Material(Shader.Find("Custom/Blind")); //Create a copy of the provided material
         render_texture = new RenderTexture(1280, 720, 1); //Create a new render texture
+        material.SetTexture("_MainTex", render_texture); //Set the material instance to use the texture we wrote to
+        GetComponentInParent<Renderer>().material = material; //Set the object to use this instance of the material
+    }
+
+    void Update() {
+        transform.LookAt(target);
     }
 
     void OnRenderImage(RenderTexture src, RenderTexture dest) {
         Graphics.Blit(src, render_texture); //Write to the texture
-        if (!GameManager.blind) {
-            material.SetTexture("Texture", render_texture); //Set the material instance to use the texture we wrote to
-            obj_renderer.material = material; //Set the object to use this instance of the material
-        }
     }
 }
