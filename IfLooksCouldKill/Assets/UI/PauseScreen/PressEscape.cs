ccadfe78566979831b3ef1de7a2379f62b18ef40
Jo Chung <chjoanna@163.com>
2022-05-12 23:27:05 -0400

    Added escape quit to end screen

---
+++ IfLooksCouldKill/Assets/UI/PauseScreen/PressEscape.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressEscape : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))//Input.GetKeyDown(KeyCode.Escape) || 
+        {
+            QuitGame();
+        }
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
